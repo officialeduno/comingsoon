5674709e76af3bfc13dbd04f4701525614e37816
Eduno <officialeduno@gmail.com>
2023-06-07 01:12:14 +0530

    Favicon changed

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ export default function Home() {
                         <meta name="viewport"
                               content="width=device-width, initial-scale=1.0"
                         />
-                        <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png" />
+                        <link rel="icon" type="image/png" sizes="32x32" href="eduno-white.png" />
                   </Head>
 
                   <main className={styles.main}>
